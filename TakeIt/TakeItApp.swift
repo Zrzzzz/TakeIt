cfa939324c7260352cd1503f70221ed3f4ecb94c
Zrzzzz <1160026659@qq.com>
2021-05-27 11:01:46 +0800

    Initial Commit

---
+++ TakeIt/TakeItApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeItApp.swift
+//  TakeIt
+//
+//  Created by Zr埋 on 2021/5/27.
+//
+
+import SwiftUI
+
+@main
+struct TakeItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
